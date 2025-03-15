de33f72740b4933bf89c26c69fff2b4f04426aac
Guest User <guestuser@Ganeshs-MacBook-Pro.local>
2025-03-14 19:00:30 -0500

    first commit

---
+++ src/main/java/com/example/bdd_automation_core_new/BddAutomationCoreNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdd_automation_core_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddAutomationCoreNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddAutomationCoreNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bdd_automation_core_new/BddAutomationCoreNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdd_automation_core_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddAutomationCoreNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
